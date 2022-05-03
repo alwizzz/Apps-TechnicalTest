ee12e30002c1d3b9c79128142dc2f8a209d40513
alwizzz <alwiza21@gmail.com>
2022-05-03 16:33:39 +0700

    create detail page

--- app2-crud/ProductList_detail.php
+++ app2-crud/ProductList_detail.php
@@ -1,3 +1,13 @@
+<?php 
+    $json_file = file_get_contents("./ProductList_data.json");
+    $json = json_decode($json_file, true);
+
+    $data = $json[$_GET['key']];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +18,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    Product Detail Page
+
+<div class="container mt-3">
+    <div class="col">
+        <div class="row">
+            <div class="col-md-10 col-12">
+                <h1>Detail of <?= $data['name']; ?></h1>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Producer:</b> <?= $data['producer']; ?></li>
+                    <li class="list-group-item"><b>Processor:</b> <?= $data['processor']; ?></li>
+                    <li class="list-group-item"><b>RAM:</b> <?= $data['RAM']; ?> GB</li>
+                    <li class="list-group-item"><b>Display Size:</b> <?= $data['displaySize']?> inch</li>
+                    <li class="list-group-item"><b>Display Resolution:</b> <?= $data['displayResolution']?></li>
+                    <li class="list-group-item"><b>Storage:</b> <?= "{$data['storageModel']} {$data['storageCapacity']}" ?> GB</li>
+                    <li class="list-group-item"><b>Weight:</b> <?= $data['weight']?> Kg</li>
+                    <li class="list-group-item"><b>Width:</b> <?= $data['width']?> Cm</li>
+                    <li class="list-group-item"><b>Height:</b> <?= $data['height']?> Cm</li>
+                    <li class="list-group-item"><b>Thickness:</b> <?= $data['thickness']?> Cm</li>
+                </ul>
+                <br>
+                <a href="ProductList_index.php" class="btn btn-success">Back to Home</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+</div>
+
+
+
+
+
 </body>
 </html>
--- app2-crud/ProductList_index.php
+++ app2-crud/ProductList_index.php
@@ -1,7 +1,7 @@
 <?php 
     $json_file = file_get_contents("./ProductList_data.json");
     $json = json_decode($json_file, true);
-    // print_r($json);
+    // print_r($_GET['a']);
 
 ?>
 
@@ -17,12 +17,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-Product List Page
 
-<div class="container d-flex flex-column">
-    <h1>Laptop List</h1>
+<div class="container mt-3 d-flex flex-column">
+    <h1>Product List</h1>
 
-    <button class="btn btn-primary mt-2 md-2" style="width: 100px">Add</button>
+    <button class="btn btn-success mt-2 md-2" style="width: 100px">Add</button>
 
     <table class="table table-stripped table-hover">
     <thead>
@@ -39,11 +38,16 @@ Product List Page
         <?php $counter=1; foreach( $json as $key => $array ) :?>
             <tr>
                 <th scope="row"><?= $counter++ ?></th>
-                <td><?= str_replace("-", " ", $key); ?></td>
+                <td><?= $array['name'] ?></td>
                 <td><?= $array['processor'] ?></td>
                 <td><?= $array['RAM'] ?> GB</td>
                 <td><?= "{$array['storageModel']} {$array['storageCapacity']}" ?> GB</td>
-                <td><button>test</button></td>
+                <td>
+
+                    <a class="btn btn-info" href="ProductList_detail.php?key=<?= $key;?>">Detail</a>
+                    <a class="btn btn-warning" href="ProductList_edit.php">Edit</a>
+                    <a class="btn btn-danger">Remove</a>
+                </td>
             </tr>
         <?php endforeach; ?>
     </tbody>
